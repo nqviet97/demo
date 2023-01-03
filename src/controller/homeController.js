e4612ca58194c7ca9a5f835286142bcfe315180a
nqviet97 <nqviet97@gmail.com>
2023-01-03 17:16:54 +0700

    video11 users

---
+++ src/controller/homeController.js
@@ -0,0 +1,16 @@
+import pool from '../configs/connectDB';
+let getHomepage = async (req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM users');
+
+    return res.render('index.ejs', { dataUser: rows, test: 'abc string test' })
+}
+
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id;
+    let [user] = await pool.execute(`select * from users where id = ?`, [userId]);
+    return res.send(JSON.stringify(user))
+}
+
+module.exports = {
+    getHomepage, getDetailPage
+}
---
+++ src/route/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+import homeController from '../controller/homeController';
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage);
+    router.get('/detail/user/:id', homeController.getDetailPage)
+    router.get('/about', (req, res) => {
+        res.send(`I'm Eric!`)
+    })
+    return app.use('/', router)
+}
+export default initWebRoute;
